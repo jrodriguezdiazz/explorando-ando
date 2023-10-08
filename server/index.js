18bd8c581796368c184cf93101e5d2be1bee51ea
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-10-08 16:31:57 -0400

    chore: improve server log

--- server/index.js
+++ server/index.js
@@ -40,5 +40,5 @@ app.post('/values', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running\nhttp://localhost:${PORT}`);
 });
