0acb04745c1b60bcaed7e4b91b010f2a79571c9d
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-12-17 18:35:54 -0400

    chore: add layouts

---
+++ client/src/components/layout/RestrictRoute.js
@@ -0,0 +1,31 @@
+import { isAuthenticated } from '@/utils/jwtUtil';
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router-dom';
+
+const RestrictRoute = ({ component: Component, layout: Layout, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Redirect
+          to={{
+            pathname: '/',
+            state: { from: props.location },
+          }}
+        />
+      ) : (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )
+    }
+  />
+);
+
+RestrictRoute.propTypes = {
+  component: PropTypes.any,
+  layout: PropTypes.any,
+  location: PropTypes.any,
+};
+
+export default RestrictRoute;
