7579cae56d9254620587b3e23b77112225822bf4
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-12-17 18:35:29 -0400

    chore: update rendering (react 17)

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
+import history from '@/utils/history';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {Router} from 'react-router-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<React.StrictMode>
-  <App />
-</React.StrictMode>);
+const root = document.getElementById('root');
+
+ReactDOM.render(<React.StrictMode>
+    <Router history={history}>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  root
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
